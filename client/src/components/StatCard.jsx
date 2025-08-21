0402aee28cc71adfe1aeeb2ff4197216fd485f74
Brian Achira <ajujubrio@gmail.com>
2025-08-21 17:29:43 +0300

    Font Updates

--- client/src/components/StatCard.jsx
+++ client/src/components/StatCard.jsx
@@ -2,10 +2,10 @@ const StatCard = ({ title, value }) => {
   return (
     <div className="bg-black border border-gray-700 rounded-xl p-4 text-center text-white  mx-auto shadow-md">
       {/* Title */}
-      <h3 className="text-2xl text-gray-300">{title}</h3>
+      <h3 className="text-xl text-gray-300">{title}</h3>
 
       {/* Value */}
-      <p className="text-emerald-500 text-3xl font-bold mt-1">{value}</p>
+      <p className="text-emerald-500 text-2xl font-bold mt-1">{value}</p>
     </div>
   );
 };
